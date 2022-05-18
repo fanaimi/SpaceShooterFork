50923d758fa65cab53fdc7756c4f98c8ae514ca1
sayxli <sayali13579@gmail.com>
2022-05-18 15:58:50 -0700

    Revert "Script Change to PlayerMoving"
    This reverts commit 3cf763bcf923b942a675cd5da0d511122447678a.

--- Assets/Space Shooter Template FREE/Scripts/DirectMoving.cs
+++ Assets/Space Shooter Template FREE/Scripts/DirectMoving.cs
@@ -5,17 +5,14 @@ using UnityEngine;
 /// <summary>
 /// This script moves the attached object along the Y-axis with the defined speed
 /// </summary>
-public class DirectMoving : MonoBehaviour
-{
+public class DirectMoving : MonoBehaviour {
 
     [Tooltip("Moving speed on Y axis in local space")]
     public float speed;
 
-
     //moving the object with the defined speed
     private void Update()
     {
-        transform.Translate(Vector3.up * speed * Time.deltaTime);
-
+        transform.Translate(Vector3.up * speed * Time.deltaTime); 
     }
 }
