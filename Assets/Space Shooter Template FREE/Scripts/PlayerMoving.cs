2f8faab906b2afdc25a279c4e7843255442df412
cyber6 <nakisa.donnelly@gmail.com>
2022-05-18 15:50:21 -0700

    moving the player with WASD keys

--- Assets/Space Shooter Template FREE/Scripts/PlayerMoving.cs
+++ Assets/Space Shooter Template FREE/Scripts/PlayerMoving.cs
@@ -19,8 +19,8 @@ public class PlayerMoving : MonoBehaviour {
     [Tooltip("offset from viewport borders for player's movement")]
     public Borders borders;
     Camera mainCamera;
-    bool controlIsActive = true; 
-
+    bool controlIsActive = true;
+    public int movementSpeed = 15;
     public static PlayerMoving instance; //unique instance of the script for easy access to the script
 
     private void Awake()
@@ -37,16 +37,21 @@ public class PlayerMoving : MonoBehaviour {
 
     private void Update()
     {
-        if (controlIsActive)
-        {
+
+        if (!controlIsActive)
+            return;
+
 #if UNITY_STANDALONE || UNITY_EDITOR    //if the current platform is not mobile, setting mouse handling 
 
-            if (Input.GetMouseButton(0)) //if mouse button was pressed       
-            {
-                Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition); //calculating mouse position in the worldspace
-                mousePosition.z = transform.position.z;
-                transform.position = Vector3.MoveTowards(transform.position, mousePosition, 30 * Time.deltaTime);
-            }
+        var movementDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += movementDirection.normalized * movementSpeed * Time.deltaTime;
+
+        if (Input.GetMouseButton(0)) //if mouse button was pressed       
+        {
+            Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition); //calculating mouse position in the worldspace
+            mousePosition.z = transform.position.z;
+            transform.position = Vector3.MoveTowards(transform.position, mousePosition, 30 * Time.deltaTime);
+        }
 #endif
 
 #if UNITY_IOS || UNITY_ANDROID //if current platform is mobile, 
@@ -65,7 +70,7 @@ public class PlayerMoving : MonoBehaviour {
                 Mathf.Clamp(transform.position.y, borders.minY, borders.maxY),
                 0
                 );
-        }
+        
     }
 
     //setting 'Player's' movement borders according to Viewport size and defined offset
