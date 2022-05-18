9786a86b8f20da3ec324158dc11883113b019676
Jason Dedrick <maskryashkaproductions@gmx.com>
2022-05-18 18:51:07 -0500

    FastUpButtonArrayUI for faster shooting

---
+++ Assets/Space Shooter Template FREE/Scripts/ShottRapidUp.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShottRapidUp : MonoBehaviour
+{
+    [SerializeField] public GameObject[] m_FastUps;
+
+    private void FasterShoot(){
+        if(GameObject == null){
+            return;
+        }
+    }
+}
